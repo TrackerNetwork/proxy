a5391f6bfd18a8d8294c1990253efe11b0e556cf
bladefist <matthewphawkins@gmail.com>
2017-08-13 21:18:22 +0000

    add monitoring

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 require('pmx').init({
-  http : true
+  http : true,
+  network: true
 });
 
 var http = require('http');
 var url = require('url');
 var request = require('request');
 var config = require('config');
+var probe = require('pmx').probe();
+var StatsD = require('node-statsd'),
+      client = new StatsD();
 
 http.createServer(onRequest).listen(80);
 
+var counter = 0;
+var statsd = 0;
 var ips = config.get('ips');
 
 console.log('Loading on IPs: ' + ips);
@@ -19,14 +25,20 @@ function onRequest(req, res) {
       
     var ip = ips[Math.floor(Math.random() * ips.length)];
    
-    console.log('Proxying on ip: ' + ip + ' -- ' + req.url);
+    //console.log('Proxying on ip: ' + ip + ' -- ' + decodeURIComponent(req.url));
     
     if (queryData.url) {
+	counter++;
+	statsd++;
+	
+	//console.log('Proxying on ip: ' + ip + ' -- ' + queryData.url);	
+	
         var internalRequest = request({
             url: queryData.url,
 	    family: 6,
 	    localAddress: ip,	   
-            headers: config.get('headers')[0]
+            headers: config.get('headers')[0],
+	    time: true
         });
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -36,13 +48,28 @@ function onRequest(req, res) {
 	  .on('error', function(e) {
            console.log('error: '+ e);
 	   res.statusCode = 500;
-	   res.end('error 500');	    
-          })
-	  .pipe(res);
+	   res.end('error 500: ' + e);	    
+          }) 
+          .pipe(res);
+   
     }
     else {
         //res.end('No url found');
 	res.end('loaderio-1e4ac91768b2952d177a51207bc4e3ac');
     }
 }
+
+var metric = probe.metric({
+  name    : 'Requests',
+  value   : function() {
+    return counter;
+  }
+});
+
+var last = 0; 
+var timer = setInterval(function() { 
+   client.increment('proxy_request', statsd - last);       
+   last = statsd;
+}, 1000);
+
 
